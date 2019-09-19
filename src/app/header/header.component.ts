f51fa8cf82b3a35a5a2eca57e0855f4527df7305
Adam Corlett <adamcorlett@Adams-MacBook-Pro.local>
2019-09-19 17:29:11 +0100

    nav styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,26 +8,65 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   bgStyle: string;
+  bookFontWeight: string;
+  popularFontWeight: string;
+  latestFontWeight: string;
+
+  bookColor: string;
+  popularColor: string;
+  latestColor: string;
 
   constructor() {
     this.bgStyle = 'new_york_busy.jpeg';
+    this.bookFontWeight = 'normal';
+    this.popularFontWeight = 'normal';
+    this.latestFontWeight = 'normal';
+
    }
 
   changeBackgroundImageHome(){
     this.bgStyle = 'new_york_busy.jpeg'
+    this.bookFontWeight = 'normal';
+    this.popularFontWeight = 'normal';
+    this.latestFontWeight = 'normal';
+
+    this.bookColor = 'white';
+    this.popularColor = 'white';
+    this.latestColor = 'white';
   }
   
 
   changeBackgroundImageBooks() {
     this.bgStyle = 'books.jpeg';
+    this.bookFontWeight = 'bold';
+    this.popularFontWeight = 'normal';
+    this.latestFontWeight = 'normal';
+
+    this.bookColor = 'gold';
+    this.popularColor = 'white';
+    this.latestColor = 'white';
   }
 
   changeBackgroundImagePopular() {
     this.bgStyle = 'popular.jpeg';
+    this.popularFontWeight =  'bold'
+    this.bookFontWeight = 'normal';
+    this.latestFontWeight = 'normal';
+
+    this.popularColor = 'gold';
+    this.bookColor = 'white';
+    this.latestColor = 'white';
   }
 
   changeBackgroundImageStories() {
     this.bgStyle = 'stories.jpg';
+    this.latestFontWeight =  'bold'
+    this.bookFontWeight = 'normal';
+    this.popularFontWeight = 'normal';
+
+    this.latestColor = 'gold';
+    this.popularColor = 'white';
+    this.bookColor = 'white';
   }
 
   ngOnInit() {
