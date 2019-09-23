1927ce1db3915a523ea531e5fde14d32912a79b2
Kelly Sherburn <kellysherburn@Kellys-MacBook-Pro.local>
2019-09-23 17:11:07 +0100

    styled component headers, pagination and added scroll to top to pagination

--- src/app/book-reviews/book-reviews.component.ts
+++ src/app/book-reviews/book-reviews.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, HostListener } from '@angular/core';
 import { APIService } from '../services/api.service';
 import { MatPaginator} from '@angular/material/paginator';
 import{MatTableDataSource, MatSort} from '@angular/material';
@@ -24,6 +24,10 @@ export class BookReviewsComponent implements OnInit{
   @ViewChild(MatSort, {static:false}) sort: MatSort;
   @ViewChild(MatPaginator, {static:false}) paginator:MatPaginator;
   searchKey: string;
+
+
+ 
+
 
   constructor(private apiService : APIService){}
 
@@ -52,5 +56,39 @@ export class BookReviewsComponent implements OnInit{
     this.listData.filter = this.searchKey.trim().toLowerCase();
   }
 
+  //when we click next on our pagination arrow the code below will scroll back to top of component page
+
+  isShow: boolean;
+  topPosToStartShowing = 100;
+
+  @HostListener('window:scroll')
+
+
+  //this method checks where you are on the page against the document body and is used to compare how far is needed to scroll when the scrollToTop method is run
+  checkScroll() {
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // scroll up on window to the top of the component page 
+  gotoTop() {
+    window.scroll({ 
+      top: 954, 
+      left: 0, 
+      behavior: 'smooth',
+    });
+  }
+
+
+  
+ 
 }
 
